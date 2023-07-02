02857dec7b3a741019f938864cdef177dc80afaf
Dylan Harding <dmharding34@gmail.com>
2023-07-01 22:41:09 -0600

    Final_Product

--- backend/backend_app.py
+++ backend/backend_app.py
@@ -12,11 +12,32 @@ POSTS = [
 
 @app.route('/api/posts', methods=['GET'])
 def get_posts():
-    return jsonify(POSTS)
+    """This is the function that shows all the posts but queries can be
+    inputted to sort how the posts are shown"""
+    sort = request.args.get('sort')
+    direction = request.args.get('direction')
+    if sort is None or direction is None:
+        return jsonify(POSTS)
+
+    if sort == 'title' and direction == 'asc':
+        sorted_posts = sorted(POSTS, key=lambda x: x['title'])
+        return jsonify(sorted_posts)
+
+    if sort == 'title' and direction == 'desc':
+        sorted_posts = sorted(POSTS, key=lambda x: x['title'], reverse=True)
+        return jsonify(sorted_posts)
+    if sort == 'content' and direction == 'asc':
+        sorted_posts = sorted(POSTS, key=lambda x: x['content'])
+        return jsonify(sorted_posts)
+
+    if sort == 'content' and direction == 'desc':
+        sorted_posts = sorted(POSTS, key=lambda x: x['content'], reverse=True)
+        return jsonify(sorted_posts)
 
 
 @app.route('/api/posts', methods=['POST'])
 def add_post():
+    """This route adds a post to the list of blog posts"""
     post_ids = []
     new_id = 0
     for post in POSTS:
@@ -31,6 +52,7 @@ def add_post():
 
 
 def find_post_by_id(post_id):
+    """This is a simple function that was created so that code wasn't repeated"""
     for post in POSTS:
         if post['id'] == int(post_id):
             return post
@@ -38,6 +60,7 @@ def find_post_by_id(post_id):
 
 @app.route('/api/posts/<id>', methods=['DELETE'])
 def delete_post(id):
+    """This route finds the post with the inputted id and deletes it"""
     post = find_post_by_id(id)
 
     if post is None:
@@ -50,6 +73,34 @@ def delete_post(id):
             del (POSTS[counter])
 
     return jsonify(POSTS)
+
+
+@app.route('/api/posts/<id>', methods=['PUT'])
+def update_post(id):
+    """This route updates the post with the new inputted info"""
+    post = find_post_by_id(id)
+
+    if post is None:
+        return jsonify("Error: Post not found"), 404
+
+    new_data = request.get_json()
+    post.update(new_data)
+
+    return jsonify(POSTS)
+
+
+@app.route('/api/posts/search', methods=['GET'])
+def search():
+    """This route searches through the posts and shows the related info to what was inputted"""
+    title = request.args.get('title')
+    content = request.args.get('content')
+    posts_to_return = []
+    if title is None or content is None:
+        return posts_to_return
+    for post in POSTS:
+        if title in post['title'] or content in post['content']:
+            posts_to_return.append(post)
+    return jsonify(posts_to_return)
 
 
 if __name__ == '__main__':
