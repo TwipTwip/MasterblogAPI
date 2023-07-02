730aee0ee9569f5a995f068de275b470a88b33bb
Dylan Harding <dmharding34@gmail.com>
2023-07-01 20:20:10 -0600

    Added_add_post_and_delete_post

--- backend/backend_app.py
+++ backend/backend_app.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 from flask_cors import CORS
 
 app = Flask(__name__)
@@ -12,6 +12,43 @@ POSTS = [
 
 @app.route('/api/posts', methods=['GET'])
 def get_posts():
+    return jsonify(POSTS)
+
+
+@app.route('/api/posts', methods=['POST'])
+def add_post():
+    post_ids = []
+    new_id = 0
+    for post in POSTS:
+        post_ids.append(post['id'])
+    new_id = max(post_ids) + 1
+    new_post = request.get_json()
+    if 'title' not in new_post or 'content' not in new_post:
+        return jsonify("Error: Post missing title or content"), 404
+    new_post['id'] = new_id
+    POSTS.append(new_post)
+    return jsonify(POSTS)
+
+
+def find_post_by_id(post_id):
+    for post in POSTS:
+        if post['id'] == int(post_id):
+            return post
+
+
+@app.route('/api/posts/<id>', methods=['DELETE'])
+def delete_post(id):
+    post = find_post_by_id(id)
+
+    if post is None:
+        return jsonify("Error: Post not found"), 404
+
+    counter = -1
+    for info in POSTS:
+        counter += 1
+        if info['id'] == int(post['id']):
+            del (POSTS[counter])
+
     return jsonify(POSTS)
 
 
